4315085fbae1297593a8dfb77f484d80f945d0b8
Agata P <43980942+agataporwit@users.noreply.github.com>
2023-08-03 12:34:07 -0700

    Initial Commit

---
+++ LORT Converter/LORT_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LORT_ConverterApp.swift
+//  LORT Converter
+//
+//  Created by Agata Porwit on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LORT_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

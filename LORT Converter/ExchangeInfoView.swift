9ab655679d8f43daecd2bb9cae76d65367f6eba9
Agata P <43980942+agataporwit@users.noreply.github.com>
2023-11-09 12:14:52 -0800

    added @Environment property, dismiss exchange info screen, minor design changes

--- LORT Converter/ExchangeInfoView.swift
+++ LORT Converter/ExchangeInfoView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct ExchangeInfoView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         ZStack {
             Image("parchment")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
-                .background(.black)
+                .padding(2)
+                //.background(.black)
             VStack{
                     //title
                     Text ("Exchange Rates")
                         .font(.largeTitle).bold().italic()
-                        .foregroundColor(.black)
+                       
                     //body text
                     Text ("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                         .font(.title3)
-                        .foregroundColor(.black)
-                        .padding(.leading)
+                        .padding(20)
             
                 //exchange rates
                 ExchangeRateView(leftImage: "goldpiece", text: "1 Gold Piece = 4 Gold Pennies ", rightImage: "goldpenny")
@@ -34,7 +36,17 @@ struct ExchangeInfoView: View {
                 
                 ExchangeRateView(leftImage: "silverpenny", text: "1 Silver Pennny = 100 Copper Pennies", rightImage: "copperpenny")
                 //button
+                
+                Button("Done") {
+                    dismiss()
+                }
+                .font(.largeTitle).bold()
+                .padding(8)
+                .foregroundColor(.yellow)
+                .background(.brown)
+                .cornerRadius(10)
             }
+            .foregroundColor(.black)
             
         }
     }
@@ -43,5 +55,6 @@ struct ExchangeInfoView: View {
 struct ExchangeInfoView_Previews: PreviewProvider {
     static var previews: some View {
         ExchangeInfoView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }

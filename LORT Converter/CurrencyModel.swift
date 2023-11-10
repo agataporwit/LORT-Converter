8a92e7810453d866c8c87d02cc6149d2c5a14280
Agata P <43980942+agataporwit@users.noreply.github.com>
2023-11-09 17:27:12 -0800

    Finished icon grid detection, added tap detection, show exchange info on currency screen

---
+++ LORT Converter/CurrencyModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyModel.swift
+//  LORT Converter
+//
+//  Created by Agata Porwit on 11/9/23.
+//
+
+import Foundation
---
+++ LORT Converter/IconGrid.swift
@@ -0,0 +1,18 @@
+//
+//  IconGrid.swift
+//  LORT Converter
+//
+//  Created by Agata Porwit on 11/9/23.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconGrid()
+}
